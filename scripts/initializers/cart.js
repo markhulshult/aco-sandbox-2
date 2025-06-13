061ab472ed9bacab803c0731c8ccfd1c486fb618
markhulshult <markhulshult@gmail.com>
2025-06-13 16:54:14 -0400

    Initialize themes

---
+++ scripts/initializers/cart.js
@@ -0,0 +1,18 @@
+import { initializers } from '@dropins/tools/initializer.js';
+import { initialize, setFetchGraphQlHeaders } from '@dropins/storefront-cart/api.js';
+import { initializeDropin } from './index.js';
+import { fetchPlaceholders } from '../commerce.js';
+import { getHeaders } from '../configs.js';
+
+await initializeDropin(async () => {
+  setFetchGraphQlHeaders((prev) => ({ ...prev, ...getHeaders('cart') }));
+
+  const labels = await fetchPlaceholders();
+  const langDefinitions = {
+    default: {
+      ...labels,
+    },
+  };
+
+  return initializers.mountImmediately(initialize, { langDefinitions });
+})();
